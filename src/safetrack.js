386eca1842619a92619d94effa88ec271c034ba1
Alex Lindeman <aelindeman@users.noreply.github.com>
2017-01-12 14:00:52 -0500

    update safetrack schedule
    using https://www.wmata.com/about/news/safetrack-schedule-2017.cfm

--- src/safetrack.js
+++ src/safetrack.js
@@ -13,140 +13,44 @@
 	Safetrack.prototype = {
 		events: [
 			{
-				"location": "Ballston-MU to East Falls Church",
-				"description": "Single-tracking",
-				"startDate": "2016-06-04T00:00:00-04:00",
-				"endDate": "2016-06-16T23:59:59-04:00",
-				"stationsAffected": ["K04", "K05"],
-				"linesAffected": ["OR", "SV"]
+				"location": "Rosslyn - Pentagon",
+				"description": "Blue Line will only operate between Franconia-Springfield and National Airport",
+				"startDate": "2017-02-11T00:00:00-05:00",
+				"endDate": "2017-02-28T23:59:59-05:00",
+				"stationsAffected": ["C05", "C06", "C07"],
+				"linesAffected": ["BL"]
 			},
 			{
-				"location": "Eastern Market to Minnesota Ave and Benning Road",
-				"description": "Shutdown",
-				"startDate": "2016-06-18T00:00:00-04:00",
-				"endDate": "2016-07-03T23:59:59-04:00",
-				"stationsAffected": ["D06", "D07", "D08", "D09", "G01"],
-				"linesAffected": ["OR", "SV", "BL"]
-			},
-			{
-				"location": "Ronald Reagan Washington National Airport to Braddock Rd",
-				"description": "Shutdown",
-				"startDate": "2016-07-05T20:00:00-04:00",
-				"endDate": "2016-07-11T23:59:59-04:00",
-				"stationsAffected": ["C10", "C12"],
-				"linesAffected": ["YL", "BL"]
-			},
-			{
-				"location": "Ronald Reagan Washington National Airport to Pentagon City",
-				"description": "Shutdown",
-				"startDate": "2016-07-12T00:00:00-04:00",
-				"endDate": "2016-07-18T23:59:59-04:00",
-				"stationsAffected": ["C10", "C09", "C08"],
-				"linesAffected": ["YL", "BL"]
-			},
-			{
-				"location": "Ballston-MU to East Falls Church",
-				"description": "Single-tracking",
-				"startDate": "2016-07-20T00:00:00-04:00",
-				"endDate": "2016-07-31T23:59:59-04:00",
-				"stationsAffected": ["K04", "K05"],
-				"linesAffected": ["OR", "SV"]
-			},
-			{
-				"location": "Takoma to Silver Spring",
-				"description": "Single-tracking",
-				"startDate": "2016-08-01T00:00:00-04:00",
-				"endDate": "2016-08-07T23:59:59-04:00",
-				"stationsAffected": ["B07", "B08"],
-				"linesAffected": ["RD"]
-			},
-			{
-				"location": "Shady Grove to Twinbrook",
-				"description": "Single-tracking",
-				"startDate": "2016-08-09T00:00:00-04:00",
-				"endDate": "2016-08-18T23:59:59-04:00",
-				"stationsAffected": ["A15", "A14", "A13"],
-				"linesAffected": ["RD"]
-			},
-			{
-				"location": "Franconia-Springfield to Van Dorn Street",
-				"description": "Single-tracking",
-				"startDate": "2016-08-20T00:00:00-04:00",
-				"endDate": "2016-09-05T23:59:59-04:00",
-				"stationsAffected": ["J03", "J02"],
+				"location": "Braddock Road - Huntington/Van Dorn Street",
+				"description": "Single-tracking will reduce Yellow and Blue Line service",
+				"startDate": "2017-03-04T00:00:00-05:00",
+				"endDate": "2017-04-09T23:59:59-04:00",
+				"stationsAffected": ["C12", "C13", "C14", "C15", "J02"],
 				"linesAffected": ["BL", "YL"]
 			},
 			{
-				"location": "Vienna/Fairfax-GMU to West Falls Church-VT/UVA",
-				"description": "Single-tracking",
-				"startDate": "2016-09-09T00:00:00-04:00",
-				"endDate": "2016-10-20T23:59:59-04:00",
-				"stationsAffected": ["K08", "K07", "K06"],
+				"location": "Greenbelt - College Park",
+				"description": "Single-tracking will reduce Green Line service",
+				"startDate": "2017-04-01T20:00:00-04:00",
+				"endDate": "2017-04-30T23:59:59-04:00",
+				"stationsAffected": ["E10", "E09"],
+				"linesAffected": ["GR"]
+			},
+			{
+				"location": "Minnesota Avenue - New Carrollton",
+				"description": "Single-tracking will reduce Orange Line service",
+				"startDate": "2017-05-01T00:00:00-04:00",
+				"endDate": "2017-05-31T23:59:59-04:00",
+				"stationsAffected": ["D09", "D10", "D11", "D12", "D13"],
 				"linesAffected": ["OR"]
 			},
 			{
-				"location": "Fort Totten to NoMa-Gallaudet U",
-				"description": "Shutdown",
-				"startDate": "2016-10-10T00:00:00-04:00",
-				"endDate": "2016-11-01T23:59:59-04:00",
-				"stationsAffected": ["B06", "E06", "B05", "B04", "B35"],
+				"location": "Shady Grove - Twinbrook",
+				"description": "Single-tracking will reduce Red Line service",
+				"startDate": "2017-06-01T00:00:00-04:00",
+				"endDate": "2017-06-30T23:59:59-04:00",
+				"stationsAffected": ["A15", "A14", "A13"],
 				"linesAffected": ["RD"]
-			},
-			{
-				"location": "East Falls Church to West Falls Church-VT/UVA",
-				"description": "Single-tracking",
-				"startDate": "2016-11-02T22:00:00-04:00",
-				"endDate": "2016-11-11T23:59:59-05:00",
-				"stationsAffected": ["K05", "K06"],
-				"linesAffected": ["OR", "SV"]
-			},
-			{
-				"location": "Greenbelt to College Park-U of MD",
-				"description": "Single-tracking",
-				"startDate": "2016-11-12T00:00:00-05:00",
-				"endDate": "2016-12-06T23:59:59-05:00",
-				"stationsAffected": ["E10", "E09"],
-				"linesAffected": ["GR", "YL"]
-			},
-			{
-				"location": "Rosslyn to Pentagon",
-				"description": "Shutdown",
-				"startDate": "2016-12-07T00:00:00-05:00",
-				"endDate": "2016-12-16T23:59:59-05:00",
-				"stationsAffected": ["C05", "C06", "C07"],
-				"linesAffected": ["BL"]
-			},
-			{
-				"location": "Rosslyn to Pentagon",
-				"description": "Shutdown",
-				"startDate": "2016-12-17T10:00:00-05:00",
-				"endDate": "2016-12-24T23:59:59-05:00",
-				"stationsAffected": ["C05", "C06", "C07"],
-				"linesAffected": ["BL"]
-			},
-			{
-				"location": "Braddock Rd to Huntington and Van Dorn Street",
-				"description": "Single-tracking",
-				"startDate": "2017-01-02T00:00:00-05:00",
-				"endDate": "2017-01-13T23:59:59-05:00",
-				"stationsAffected": ["C12", "C13", "C14", "C15", "J02"],
-				"linesAffected": ["YL", "BL"]
-			},
-			{
-				"location": "Braddock Rd to Huntington and Van Dorn Street",
-				"description": "Single-tracking",
-				"startDate": "2017-01-22T22:00:00-05:00",
-				"endDate": "2017-02-03T23:59:59-05:00",
-				"stationsAffected": ["C12", "C13", "C14", "C15", "J02"],
-				"linesAffected": ["YL", "BL"]
-			},
-			{
-				"location": "East Falls Church to West Falls Church-VT/UVA",
-				"description": "Single-tracking",
-				"startDate": "2017-03-06T00:00:00-05:00",
-				"endDate": "2017-03-19T23:59:59-04:00",
-				"stationsAffected": ["K05", "K06"],
-				"linesAffected": ["OR", "SV"]
 			}
 		],
 
